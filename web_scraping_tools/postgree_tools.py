81b0578be57722b8d8674fd0986c332a2bb6a1b9
FcaioF <caiofranciscoferreira@outlook.com>
2023-09-10 17:28:19 -0300

    Funções refatoradas para melhor entendimento do codigo desenvolvido

--- web_scraping_tools/postgree_tools.py
+++ web_scraping_tools/postgree_tools.py
@@ -30,18 +30,141 @@ def postgree_connect():
         print('Connection failed!!\nplease check your connection file\n',e)
         return
 
-def insert_data(db_connection,data):
-    pre_processing= [ (f"'{i}',") for i in list(data.values())[:-1] ]
-    pre_processing.append(f"'{ list(data.values())[-1] }');")
-    concat_string= str('\n'.join(pre_processing))
-    insert_template= str('INSERT INTO books(DT_PROCESS_DATE,DS_BOOK_CATEGORY,DS_BOOK_TITLE,NR_BOOK_PRICE,DS_BOOK_DESCRIPTION,NR_UPC_NUMBER,TP_PRODUCT_TYPE,NR_NO_TAX_PRICE,NR_W_TAX_PRICE,NR_TOTAL_AVAILABLE,DS_BOOK_STAR) VALUES (')
-    #execute the command in the database
+#Returns all registers in the database
+def get_books(db_connection):
+    _connection = (
+        db_connection
+        .cursor()
+    )
+    (
+        _connection
+        .execute('select * from books')
 
+    )
+    return _connection.fetchall()
+
+#Returns registers based in id
+def get_books_by_id(db_connection, id):
+    _connection = (
+        db_connection
+        .cursor()
+    )
+    _select_template = (
+        """
+        SELECT * 
+        FROM books
+        WHERE ID_BOOK = %s
+        """
+    )
+    (_connection
+     .execute(_select_template, id)
+     )
+
+    return _connection.fetchall()
+
+def insert_book_data(db_connection, data):
+    _pre_processing = tuple(data.values())
+    insert_template = ("""
+    INSERT INTO books
+    (DT_PROCESS_DATE,DS_BOOK_CATEGORY,DS_BOOK_TITLE,NR_BOOK_PRICE,DS_BOOK_DESCRIPTION,NR_UPC_NUMBER,TP_PRODUCT_TYPE,NR_NO_TAX_PRICE,NR_W_TAX_PRICE,NR_TOTAL_AVAILABLE,DS_BOOK_STAR) 
+    VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+    """)
+    # execute the command in the database
     try:
         (
             db_connection
             .cursor()
-            .execute(f'{insert_template + concat_string}')
+            .execute(insert_template, _pre_processing)
+        )
+
+        # write the alteration in the database;
+        (
+            db_connection
+            .commit()
+        )
+
+        # db_connection.fetchall()
+        print('row added!')
+    except Exception as e:
+        print('error when add register in databse\n', e, '\n', _pre_processing)
+
+    db_connection.close()
+
+
+# function used to update book price based in id
+def update_book_price(db_connection, data):
+    _update_template = (
+        """
+        UPDATE books
+        SET NR_BOOK_PRICE= '%s'
+        WHERE id_book = %s
+        """
+    )
+    try:
+        (
+            db_connection
+            .cursor()
+            .execute(_update_template, data)
+        )
+
+        # write the alteration in the database;
+        (
+            db_connection
+            .commit()
+        )
+
+        db_connection.close()
+        return ('row updated!')
+    except Exception as e:
+        db_connection.close()
+        return ('error when update register in databse\n', e, '\n', data)
+
+
+
+
+# function used to update the book quantity based in id
+def update_book_quantity(db_connection, data):
+    _update_template = (
+        """
+        UPDATE books
+        SET NR_TOTAL_AVAILABLE= '%s'
+        WHERE id_book = %s
+        """
+    )
+    try:
+        (
+            db_connection
+            .cursor()
+            .execute(_update_template, data)
+        )
+
+        # write the alteration in the database;
+        (
+            db_connection
+            .commit()
+        )
+
+        db_connection.close()
+        return('row updated!')
+    except Exception as e:
+        db_connection.close()
+        return(f'error when update register in databse\n{e}\n{data}')
+
+
+
+def delete_books(db_connection,id):
+    _delete_template=(
+        """
+        DELETE 
+        FROM books 
+        WHERE id_book = %s
+        """
+    )
+    try:
+        (
+            db_connection
+            .cursor()
+            .execute(_delete_template,id)
         )
 
         #write the alteration in the database;
@@ -51,10 +174,10 @@ def insert_data(db_connection,data):
         )
 
         # db_connection.fetchall()
-        print('row added!')
+        print('row deleted!')
     except Exception as e:
-        print('error when add register in databse\n',e,'\n',pre_processing)
-        db_connection.close()
-        # print('error in line: ',)
+        print('error when delete register in databse\n',e,'\n',data)
+    db_connection.close()
+
 
 
